5c047bbc1095dbb26212d4bb2e45159070cd66e3
Laura Polaina <laurapolainac@gmail.com>
2026-03-10 20:02:22 +0100

    solo el menu con los numeros generados

--- src/PolainaLauraEjer1.java
+++ src/PolainaLauraEjer1.java
@@ -1,33 +1,22 @@
-import java.util.Scanner;
 import java.util.Random;
 
 public class PolainaLauraEjer1 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
         Random r = new Random();
 
-        // premios
+        // Generación de premios
         int gordo = r.nextInt(99999) + 1;
         int segundo = r.nextInt(99999) + 1;
         int tercero = r.nextInt(99999) + 1;
 
-        int opcion = 3;
+        // Mostrar premios del sorteo
+        System.out.println("----- SORTEO DE LOTERIA -----");
 
-        while(opcion != 0){
-
-            System.out.println("----- LOTERIA -----");
-            System.out.println("1. Comprobar decimo");
-            System.out.println("0. Salir");
-
-            opcion = sc.nextInt();
-
-
-
-
-
-        }
+        System.out.printf("Primer premio: %05d\n", gordo);
+        System.out.printf("Segundo premio: %05d\n", segundo);
+        System.out.printf("Tercer premio: %05d\n", tercero);
 
     }
 }
