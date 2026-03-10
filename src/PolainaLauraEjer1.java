6ce21168be7b54a3a6752a08aa8e9adf01e98bbc
Laura Polaina <laurapolainac@gmail.com>
2026-03-10 19:25:14 +0100

    ejercicio que muestra si has ganado o no

--- src/PolainaLauraEjer1.java
+++ src/PolainaLauraEjer1.java
@@ -13,9 +13,9 @@ public class PolainaLauraEjer1 {
         int segundo = r.nextInt(99999) + 1;
         int tercero = r.nextInt(99999) + 1;
 
-        int opcion = 3;
+        int opcion = -1;
 
-        while(opcion != 0){
+        while(opcion != 0) {
 
             System.out.println("----- LOTERIA -----");
             System.out.println("1. Comprobar decimo");
@@ -23,7 +23,7 @@ public class PolainaLauraEjer1 {
 
             opcion = sc.nextInt();
 
-            if(opcion == 1){
+            if (opcion == 1) {
 
                 int decimo;
                 int importe;
@@ -34,25 +34,19 @@ public class PolainaLauraEjer1 {
                 System.out.print("Introduce importe jugado: ");
                 importe = sc.nextInt();
 
-                if(decimo == gordo){
+                if (decimo == gordo) {
                     System.out.println("Primer premio!! Has ganado " + (importe * 20000) + "€");
-                }
-                else if(decimo == segundo){
+                } else if (decimo == segundo) {
                     System.out.println("Segundo premio!! Has ganado " + (importe * 6250) + "€");
-                }
-                else if(decimo == tercero){
+                } else if (decimo == tercero) {
                     System.out.println("Tercer premio!! Has ganado " + (importe * 2500) + "€");
-                }
-                else{
+                } else {
                     System.out.println("Lo siento no has tenido suerte");
                 }
 
                 System.out.printf("Decimo %05d\n", decimo);
             }
 
-
-
         }
-
-    }
+        }
 }
