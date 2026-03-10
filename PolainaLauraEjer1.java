39b1d9f95115d573f2f3dbcec6a44a3049911145
polainalaura <92717040+polainalaura@users.noreply.github.com>
2026-03-10 18:55:14 +0100

    Add files via upload

---
+++ PolainaLauraEjer1.java
@@ -0,0 +1,65 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class PolainaLauraEjer1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+
+        // premios
+        int gordo = r.nextInt(99999) + 1;
+        int segundo = r.nextInt(99999) + 1;
+        int tercero = r.nextInt(99999) + 1;
+
+        int opcion = -1;
+
+        while(opcion != 0){
+
+            System.out.println("----- LOTERIA -----");
+            System.out.println("1. Comprobar decimo");
+            System.out.println("2. Resumen premios");
+            System.out.println("0. Salir");
+
+            opcion = sc.nextInt();
+
+            if(opcion == 1){
+
+                int decimo;
+                int importe;
+
+                System.out.print("Introduce numero de decimo: ");
+                decimo = sc.nextInt();
+
+                System.out.print("Introduce importe jugado: ");
+                importe = sc.nextInt();
+
+                if(decimo == gordo){
+                    System.out.println("Primer premio!! Has ganado " + (importe * 20000) + "€");
+                }
+                else if(decimo == segundo){
+                    System.out.println("Segundo premio!! Has ganado " + (importe * 6250) + "€");
+                }
+                else if(decimo == tercero){
+                    System.out.println("Tercer premio!! Has ganado " + (importe * 2500) + "€");
+                }
+                else{
+                    System.out.println("Lo siento no has tenido suerte");
+                }
+
+                System.out.printf("Decimo %05d\n", decimo);
+            }
+
+            if(opcion == 2){
+
+                System.out.println("Primer premio: " + gordo);
+                System.out.println("Segundo premio: " + segundo);
+                System.out.println("Tercer premio: " + tercero);
+
+            }
+
+        }
+
+    }
+}
